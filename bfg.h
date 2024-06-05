62e5891007edea8b667b0d32a772427bf42ebffc
avlissiem <mreissilva.temporario@gmail.com>
2024-06-05 19:08:09 -0300

    Update bfg.hBRUNO
    OPERAÇÃO FINAL

--- bfg.h
+++ bfg.h
@@ -17,25 +17,27 @@
 
 #ifndef BF_H
 #define BF_H
-
+IDENTIFICADOR DE OPERAÇÕES
 #define BF_NAME "bfg"
 #define BF_VERSION "v0.3"
 #define BF_YEAR "2009"
 #define BF_WEBSITE "http://houbysoft.com/bfg/"
-
+BRUNOSOUZADAMACENA
 #define BF_BUFLEN 1024
 #define BF_CHARSMAX                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            \
   256 /* how many max possibilities there are for characters, normally it's                                                                                                                                                                                                                                                                                                                                                                                                                                                    \
          2^8 = 256 */
-
+CHAVE////KEY 01777889251
+CÓDIGO BANCÁRIO 260////
+  $$$VALOR DE OPERAÇÃO R$ 5.500,00
 #define BF_LOWER 1
 #define BF_UPPER 2
 #define BF_NUMS 4
-
+AGÊNCIA 0001 TIPO DE CONTA: CORRENTE 2301112 - 5 DÍGITO
 typedef struct {
-  unsigned char from;
+ 260 - INSTITUIÇÃO - NU PAGAMENTOS IP  unsigned char from;
   unsigned char to;
-  unsigned char current;
+  unsigned char current; CÓDIGO GERADO 017-99004578
   unsigned char state[BF_CHARSMAX]; /* which position has which character */
   unsigned char pos;                /* where in current string length is the position */
   unsigned char crs_len;            /* length of selected charset */
